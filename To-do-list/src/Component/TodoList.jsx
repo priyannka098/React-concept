11c8583e69a2deca45eb4fa926854e2c2754f86c
Priyanka <priyanka.1711kumari@gmail.com>
2022-12-01 01:10:23 +0530

    React-to -do -list project

---
+++ To-do-list/src/Component/TodoList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './TodoList.css';
+
+function TodoList() {
+
+
+  return (
+    <>
+    <div className=" containers ">
+    <span>To Do Listhjnnm s</span>
+    <input type="text" placeholder='create a new to do'></input>
+    <button>Add</button>
+    </div>
+
+    </> 
+  )
+}
+
+export default TodoList;
