a97c319d625a33f9c8ee4dc71ba978e960d6aa69
Priyanka <priyanka.1711kumari@gmail.com>
2022-12-02 11:40:17 +0530

    to do list completed

--- To-do-list/src/Component/TodoList.jsx
+++ To-do-list/src/Component/TodoList.jsx
@@ -1,18 +1,69 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import "./TodoList.css";
 
+
+
 function TodoList() {
+  const[activity,setActivity]=useState("")
+  const[listData,setListData]=useState([])
 
 
+
+const addActivity=()=>{
+  // setListData([...listData,activity]) //for asyncronous
+  // console.log(listData);
+  setListData((listData)=>{
+    const updatedList=[...listData,activity]
+    console.log(updatedList)
+    setActivity('');
+    return updatedList
+     
+  })
+}
+const removeList=(i)=>{
+  const updatedListData=listData.filter((element,id)=>{
+    return i!=id;
+  })
+  setListData(updatedListData)
+
+}
+const RemoveAll=()=>{
+  setListData([])
+}
+
+ const handleOnChange=(e)=>{
+  setActivity(e.target.value)
+
+ }
   return (
     <>
     <div className="outer-box">
     <div className=" container ">
     <span className="list">To Do List </span>
     <div>
-    <input  className="input"type="text" placeholder='Create a new to do list'></input>
-    <button className="button">Add</button>
+    <input  className="input"type="text" placeholder='Create a new to do list' value={activity} onChange={handleOnChange} ></input>
+    <button className="button" onClick={addActivity}>Add</button>
+    {listData!=[]&& listData.map((data,index)=>{
+      return(
+        <>
+        <div className="rowdata">
+        <p key={index}>
+          <div className="listdata">{data}</div>
+        </p>
+        <div >
+        <button  className=" Delete"onClick={()=>removeList(index)} >Delete</button>
+        </div>
+        </div>
+        </>
+      )
+    }
+    )}
     </div>
+
+    <div>
+    {listData.length>=1&&
+        <button className="DeleteAll" onClick={RemoveAll}>Delete All</button>}
+        </div>
     </div>
     </div>
 
